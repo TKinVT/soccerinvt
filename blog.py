1490c47f7cbf71b4c623dafe169137484be9cc6a
TK <tk@tkinvt.com>
2022-02-27 21:37:48 -0500

    added POST route for adding new blog post

--- blog.py
+++ blog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from models import Post
 
@@ -40,6 +40,26 @@ def longer():
     long_posts = Post.objects()
     header_tags = Post.sorted_tags()[:5]
     return render_template('longer.html', posts=long_posts, tags=header_tags)
+
+
+@app.route('/new', methods=['POST'])
+def new_post():
+    data = request.json
+    print(data)
+    post = Post(content=data['content'])
+
+    if 'title' in data and data['title'] is not None:
+        post.title = data['title']
+
+    if 'photo' in data and data['photo'] is not None:
+        post.photo = data['photo']
+
+    if 'tags' in data and data['tags'] is not None:
+        post.tags = data['tags']
+
+    post.save()
+
+    return ""
 
 
 if __name__ == '__main__':
